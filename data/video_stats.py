61b0832b951f94a40a996fb41a263d734b60dad1
Tidiane-junior <77800865+Tidiane-junior@users.noreply.github.com>
2026-03-05 15:53:07 +0100

    Ajout de .env dans gitignore et sauvegarder API_KEY dans le fichier

--- data/video_stats.py
+++ data/video_stats.py
@@ -1,10 +1,14 @@
 # Importer les modules necessaire
+from dotenv import load_dotenv
 import requests # requests pour faire des requêtes HTTP
 import json # json pour gérer les données JSON
+import os # os pour gérer les variables d'environnement
+from dotenv import load_dotenv # pour charger les variables d'environnement à partir d'un fichier .env
 
-# Définir la clé API et le nom d'utilisateur du canal YouTube
-API_KEY = "AIzaSyBqcM8cdBb7vI5mAkV0EJ4gGx8dOq8WRew"
-channel_handle = "MrBeast"
+load_dotenv(dotenv_path ="./.env") # Charger les variables d'environnement à partir du fichier .env
+
+API_KEY = os.getenv("API_KEY") # Récupérer la clé API à partir des variables d'environnement
+channel_handle = "MrBeast" # La chaine Youtube dont on veut récupérer les statistiques
 
 def get_playlisT_Id():
 
@@ -12,7 +16,6 @@ def get_playlisT_Id():
         # Construire l'URL de la requête depuis YouTube API pour obtenir les détails du canal YouTube
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={channel_handle}&key={API_KEY}"
 
-        # Faire la requête GET à l'API YouTube et stocker la réponse dans une variable data
         response = requests.get(url)
 
         response.raise_for_status() # Vérifier si la requête a réussi, sinon lever une exception
@@ -26,10 +29,10 @@ def get_playlisT_Id():
 
         # Je recupère les éléments de la réponse JSON pour accéder aux détails du canal YouTube
         channel_items = data["items"][0]
-        # Je récupère l'identifiant de la playlist des vidéos téléchargées du canal YouTube à partir des détails du canal
+        # Je récupère l'identifiant de la playlist des vidéos téléchargées 
         channel_playlisId = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
 
-        # print(channel_playlisId) # Afficher l'identifiant de la playlist des vidéos téléchargées du canal YouTube
+        print(channel_playlisId) 
 
         return channel_playlisId
     except requests.exceptions.RequestException as e:
