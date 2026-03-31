649eb478468baab8cd43968e2f293cb54fc96e30
Tidiane-junior <77800865+Tidiane-junior@users.noreply.github.com>
2026-03-31 15:10:17 +0200

    Update docker-compose, fernet and create file shell

--- fernet_key_gen.py
+++ fernet_key_gen.py
@@ -1,4 +1,4 @@
-#%% Generer une cle de chiffrement Fernet
+#%% Generer une cle de chiffrement Fernet pour Airflow
 from cryptography.fernet import Fernet
 print(Fernet.generate_key().decode())
 # %%
